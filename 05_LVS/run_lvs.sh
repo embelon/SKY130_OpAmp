b883424b450696155a213130c102ab5143c6d36c
embelon <78412338+embelon@users.noreply.github.com>
2023-11-21 14:59:15 +0100

    Modified script for LVS check to do extraction from layout by calling magic in batch mode

--- 05_LVS/run_lvs.sh
+++ 05_LVS/run_lvs.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-cp -u ../03_Layout/opamp_cascode*.spice .
-sed '/^C/d' < opamp_cascode_layout.spice > opamp_cascode_layout_noC.spice
-netgen -batch lvs "opamp_cascode.spice opamp_cascode" "opamp_cascode_layout_noC.spice opamp_cascode" $PDK_ROOT/sky130A/libs.tech/netgen/sky130A_setup.tcl 
 
+## copy source netlist that layout was based on 
+cp -u ../03_Layout/opamp_cascode.spice .
+
+## use magic in batch mode to extract netlist from layout
+magic -noconsole -dnull extract_netlist_noparasitics.tcl
+## sed '/^C/d' < opamp_cascode_layout.spice > opamp_cascode_layout_noC.spice
+
+## remove ext files
+rm *.ext
+
+## compare using netgen
+netgen -batch lvs "opamp_cascode.spice opamp_cascode" "opamp_cascode_noparasitics.spice opamp_cascode" $PDK_ROOT/sky130A/libs.tech/netgen/sky130A_setup.tcl
 
