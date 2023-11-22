bc0eb269fa1a01917ea6628fb25232ea0735883c
embelon <78412338+embelon@users.noreply.github.com>
2023-11-22 14:43:20 +0100

    Fixed issue with parasitic resistances not included in spice

--- 06_ParasiticsSim/extract_netlist_parasitics.tcl
+++ 06_ParasiticsSim/extract_netlist_parasitics.tcl
@@ -1,4 +1,4 @@
-box 0 0 0 0
+box values 0 0 0 0
 load ../03_Layout/opamp_cascode.mag
 select top cell
 expand
@@ -7,13 +7,17 @@ load opamp_cascode_flat
 cellname delete opamp_cascode
 cellname rename opamp_cascode_flat opamp_cascode
 select top cell
+extract do resistance
 extract all
 ext2sim labels on
 ext2sim
 extresist tolerance 10
-extresist
+extresist all
 ext2spice lvs
-ext2spice cthresh 0
+ext2spice cthresh 0.01
+ext2spice rthresh 100
 ext2spice extresist on
-ext2spice -d -o opamp_cascode.spice
+ext2spice resistor tee on
+ext2spice format ngspice
+ext2spice -d -p . -o opamp_cascode.spice
 quit -noprompt
